fc24320f2fd26d50b6f2744452d8328fa4c207cb
Suqah <getretardisfree@gmail.com>
2022-09-06 14:13:41 +0700

    Add constatns

---
+++ components/api_handler/common/constants.py
@@ -0,0 +1,5 @@
+
+# df content
+CONTENT_TYPE = 'content_type'
+UNKNOWN_CONTENT = 'Unknown'
+POST_CONTENT = 'Post'
---
+++ components/common/constants.py
@@ -0,0 +1,6 @@
+
+DATE_FORMAT = '%d.%m.%Y'
+DF_FORMAT = 'csv'
+SPLIT = '__'
+SUB_SPLIT = '+'
+FILE_NAME = '{classtype}'+SPLIT+'{content}'+'.'+'{fileformat}'
